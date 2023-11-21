db211110b0e5591e164ecebe04c4210f0852e8cb
092vk <vk092kumar@gmail.com>
2023-11-21 12:45:42 +0530

    added signIn and signUp with email

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// document.querySelector('.toggle-btn').addEventListener('click', function() {
-//     const signInForm = document.querySelector('.sign_in');
-//     const signUpForm = document.querySelector('.sign_up');
-
-//     if (signInForm.style.display === 'block' || signInForm.style.display === '') {
-//         signInForm.style.display = 'none';
-//         signUpForm.style.display = 'block';
-//         this.textContent = 'Switch to Sign In';
-//     } 
-//     else {
-//         signInForm.style.display = 'block';
-//         signUpForm.style.display = 'none';
-//         this.textContent = 'Switch to Sign Up';
-//     }
-// });
 
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("signin-form");
